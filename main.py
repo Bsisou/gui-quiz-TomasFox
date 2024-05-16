cebaa0c96588217372c86b0f9e5a8ca319c8171f
TomasFox <161266151+TomasFox@users.noreply.github.com>
2024-05-16 21:23:42 +0000

    background resizes to fit window

--- main.py
+++ main.py
@@ -1,9 +1,40 @@
 from tkinter import *
 
-root=Tk()
-bg = PhotoImage(file="MAP.png")
-bglabel=Label(root, image=bg)
-bglabel.place(x=0, y=0)
-root.title("geography quiz")
-root.geometry( "750x500" )
+from PIL import Image, ImageTk
+
+class background(Frame):
+    def __init__(self, master, *pargs):
+        Frame.__init__(self, master, *pargs)
+
+
+
+        self.image = Image.open("MAP.png")
+        self.img_copy= self.image.copy()
+
+
+        self.background_image = ImageTk.PhotoImage(self.image)
+
+        self.background = Label(self, image=self.background_image)
+        self.background.pack(fill=BOTH, expand=YES)
+        self.background.bind('<Configure>', self._resize_image)
+
+    def _resize_image(self,event):
+
+        new_width = event.width
+        new_height = event.height
+
+        self.image = self.img_copy.resize((new_width, new_height))
+
+        self.background_image = ImageTk.PhotoImage(self.image)
+        self.background.configure(image =  self.background_image)
+
+
+
+
+root = Tk()
+root.title("Title")
+root.geometry("900x600")
+root.configure(background="black")
+bg =background(root)
+bg.pack(fill=BOTH, expand=YES)
 root.mainloop()
