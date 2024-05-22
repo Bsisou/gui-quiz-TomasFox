989dd51655f7d3b29b2e1d02f83be45a75d4924b
TomasFox <161266151+TomasFox@users.noreply.github.com>
2024-05-22 00:09:32 +0000

    first component

--- main.py
+++ main.py
@@ -28,13 +28,33 @@ class background(Frame):
         self.background_image = ImageTk.PhotoImage(self.image)
         self.background.configure(image =  self.background_image)
 
+class startpage(Frame):
+    def __init__(self, master, *pargs):
+        Frame.__init__(self, master, *pargs)
+
+        self.background = background(self)
+        self.startframe=Frame(app, self.background, padx=100, pady=100)
+        self.startframe.grid()
+        self.label = Label(self, text="Welcome to the game")
+        self.label.pack()
+
+        self.button = Button(self, text="Start", command=self.start)
+        self.button.pack()
+
+    def start(self):
+        self.background.destroy()
+        self.label.destroy()
+        self.button.destroy()
+        self.new_window = Tk()
+        self.app = Game(self.new_window)
+        self.app.pack()
+        self.new_window.mainloop()
 
 
-
-root = Tk()
-root.title("Title")
-root.geometry("900x600")
-root.configure(background="black")
-bg =background(root)
-bg.pack(fill=BOTH, expand=YES)
-root.mainloop()
+app = Tk()
+app.title("Geography quiz")
+app.geometry("900x600")
+app.configure(background="black")
+start=startpage(app)
+start.place(x=0, y=0, width=900, height=600)
+app.mainloop()
